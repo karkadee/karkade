d53c59a25cde93c0ea243bffc7c474c55babd873
Rob <robzlegz@gmail.com>
2022-02-05 20:40:27 +0200

    cats

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import Aos from "aos";
 import { AppProps } from "next/app"
+import { useEffect } from "react";
 import "../src/styles/globals.css"
 
 function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
   return <Component {...pageProps} />
 }
 
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,76 +1,115 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import productCategories from "../tests/productCategories";
 
 function Navigation() {
-  return (
-    <nav className="bg-white w-full">
-        <div className="w-full flex flex-col ss:flex-row items-center justify-between px-2 sm:px-6 py-3">
-            <div className="md:w-40 lg:w-60 mr-0 ss:mr-2 sm:mr-4 md:mr-8 mb-2 ss:mb-0">
-                <button className="outline-green-600 p-1">
-                    <Link href="/">
-                        <h1>Karkadē</h1>
-                    </Link>
-                </button>
-            </div>
+    const [showCats, setShowCats] = useState(true);
 
-            <div className="hidden sm:flex h-8 flex-1 rounded-md">
-                <input 
-                    className="rounded-l-md hidden sm:block w-full h-full outline-green-600"
-                    placeholder="meklēt"
-                />
+    const transitionNavBar = () => {
+        if(window.scrollY > 100){
+            setShowCats(true);
+        }else{
+            setShowCats(false);
+        }
+    }
+    
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavBar);
+        return () => window.removeEventListener("scroll", transitionNavBar);
+    }, []);
 
-                <button className="outline-green-600 p-1 h-full w-20 flex items-center justify-center bg-green-600 rounded-r-md">
-                    <img 
-                        className="w-5"
-                        src="/svg/search.svg" 
-                        alt="search" 
+    console.log(showCats)
+    
+    return (
+        <nav className="bg-white w-full">
+            <div className="w-full flex flex-col ss:flex-row items-center justify-between px-2 sm:px-6 py-3">
+                <div className="md:w-40 lg:w-60 mr-0 ss:mr-2 sm:mr-4 md:mr-8 mb-2 ss:mb-0">
+                    <button className="outline-green-600 p-1">
+                        <Link href="/">
+                            <h1>Karkadē</h1>
+                        </Link>
+                    </button>
+                </div>
+
+                <div className="hidden sm:flex h-8 flex-1 rounded-md">
+                    <input 
+                        className="rounded-l-md hidden sm:block w-full h-full outline-green-600"
+                        placeholder="meklēt"
                     />
-                </button>
+
+                    <button className="outline-green-600 p-1 h-full w-20 flex items-center justify-center bg-green-600 rounded-r-md">
+                        <img 
+                            className="w-5"
+                            src="/svg/search.svg" 
+                            alt="search" 
+                        />
+                    </button>
+                </div>
+                
+
+                <div className="justify-between ml-0 ss:ml-2 sm:ml-4 md:ml-8 w-full ss:w-52 sm:w-40 lg:w-60 flex items-center ss:justify-start">
+                    <button className="mr-1 sm:mr-4 outline-green-600 p-1">
+                        <Link href="/">
+                            <p className="font-bold">Sākums</p>
+                        </Link>
+                    </button>
+
+                    <button className="outline-green-600 p-1 sm:hidden">
+                        <Link href="/search">
+                            <div className="flex mr-1">
+                                <img 
+                                    className="w-5"
+                                    src="/svg/search.svg" 
+                                    alt="search" 
+                                />
+
+                                <p className="font-bold">Meklēt</p>
+                            </div>
+                        </Link>
+                    </button>
+
+                    <button className="outline-green-600 p-1">
+                        <Link href="/cart">
+                            <div className="flex">
+                                <img 
+                                    className="w-5 hidden sm:block"
+                                    src="/svg/cart.svg" 
+                                    alt="shopping cart" 
+                                />
+                                
+                                <p className="font-bold block sm:block">Grozs</p>
+                            </div>
+                        </Link>
+                    </button>
+                </div>
             </div>
-            
 
-            <div className="justify-between ml-0 ss:ml-2 sm:ml-4 md:ml-8 w-full ss:w-52 sm:w-40 lg:w-60 flex items-center ss:justify-start">
-                <button className="mr-1 sm:mr-4 outline-green-600 p-1">
-                    <Link href="/">
-                        <p className="font-bold">Sākums</p>
-                    </Link>
-                </button>
-
-                <button className="outline-green-600 p-1 sm:hidden">
-                    <Link href="/search">
-                        <div className="flex mr-1">
-                            <img 
-                                className="w-5"
-                                src="/svg/search.svg" 
-                                alt="search" 
-                            />
-
-                            <p className="font-bold">Meklēt</p>
-                        </div>
-                    </Link>
-                </button>
-
-                <button className="outline-green-600 p-1">
-                    <Link href="/cart">
-                        <div className="flex">
-                            <img 
-                                className="w-5 hidden sm:block"
-                                src="/svg/cart.svg" 
-                                alt="shopping cart" 
-                            />
-                            
-                            <p className="font-bold block sm:block">Grozs</p>
-                        </div>
-                    </Link>
-                </button>
+            <div 
+                className="hidden md:flex w-full flex-col ss:flex-row items-center justify-center px-2 sm:px-10 py-1"
+                data-aos="fade-down"
+                data-aos-offset="200"
+                data-aos-delay="50"
+                data-aos-duration="1000"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true"
+                data-aos-once="false"
+                data-aos-anchor-placement="top-center"
+            >
+                {
+                    productCategories.map((cat, i) => {
+                        return (
+                            <button 
+                                key={i}
+                                className={`outline-green-600 p-1 ${productCategories.length - 1 === i ? "" : "mr-4"}`}
+                            >
+                                <p className="font-bold hover:text-green-700 transition-colors duration-150">{cat.text}</p>
+                            </button>
+                        )
+                    })
+                }
             </div>
-        </div>
-
-        <div className="w-full flex flex-col ss:flex-row items-center justify-center px-2 sm:px-6 py-1">
-
-        </div>
-    </nav>
-  );
+        </nav>
+    );
 }
 
 export default Navigation;
