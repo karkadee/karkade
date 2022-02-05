41807b9de5d9b213f1cf99d7d061d78d7b6a5f4c
Rob <robzlegz@gmail.com>
2022-02-05 18:57:10 +0200

    navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,29 +1,32 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 function Navigation() {
   return (
-    <nav className="navigation">
-        <button>
-            <Link href="/">
-                <h1>Karkadē</h1>
-            </Link>
-        </button>
+    <nav className="w-full flex items-center justify-between px-6 py-2">
+        <div className="w-60 mr-8">
+            <button className="outline-green-600 p-1">
+                <Link href="/">
+                    <h1>Karkadē</h1>
+                </Link>
+            </button>
+        </div>
 
         <input 
-            className="navigation__input"
+            className="h-8 flex-1 outline-green-600"
+            placeholder="meklēt"
         />
 
-        <div className="navigation__links">
-            <button>
+        <div className="ml-8 w-60 flex items-center justify-start">
+            <button className="mr-4 outline-green-600 p-1">
                 <Link href="/">
-                    <p>Sākums</p>
+                    <p className="font-bold">Sākums</p>
                 </Link>
             </button>
 
-            <button>
+            <button className="outline-green-600 p-1">
                 <Link href="/cart">
-                    <p>Grozs</p>
+                    <p className="font-bold">Grozs</p>
                 </Link>
             </button>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  mode: "jit",
   theme: {
     extend: {},
   },
