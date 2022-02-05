17aa4884cab5b14f2eac4f36adaf03e68097dcd6
Rob <robzlegz@gmail.com>
2022-02-05 19:17:31 +0200

    responsive nav start

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,11 +1,11 @@
 import Head from "next/head"
 import Navigation from "../src/components/Navigation";
 
-export default function Cart() {
+export default function Search() {
   return (
     <div>
       <Head>
-        <title>Karkadē | Grozs</title>
+        <title>Karkadē | Meklēt</title>
         <meta name="description" content="Karkadē Populārākais puķu veikals Rīgā" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 function Navigation() {
   return (
-    <nav className="w-full flex items-center justify-between px-6 py-2">
-        <div className="w-60 mr-8">
+    <nav className="w-full flex items-center justify-between px-2 sm:px-6 py-2">
+        <div className="md:w-40 lg:w-60 mr-2 sm:mr-4 md:mr-8">
             <button className="outline-green-600 p-1">
                 <Link href="/">
                     <h1>Karkadē</h1>
@@ -13,20 +13,42 @@ function Navigation() {
         </div>
 
         <input 
-            className="h-8 flex-1 outline-green-600"
+            className="hidden sm:block h-8 flex-1 outline-green-600"
             placeholder="meklēt"
         />
 
-        <div className="ml-8 w-60 flex items-center justify-start">
-            <button className="mr-4 outline-green-600 p-1">
+        <div className="ml-2 sm:ml-4 md:ml-8 md:w-40 lg:w-60 flex items-center justify-start">
+            <button className="mr-1 sm:mr-4 outline-green-600 p-1">
                 <Link href="/">
                     <p className="font-bold">Sākums</p>
                 </Link>
             </button>
 
+            <button className="outline-green-600 p-1 sm:hidden">
+                <Link href="/search">
+                    <div className="flex mr-1">
+                        <img 
+                            className="w-5"
+                            src="/svg/search.svg" 
+                            alt="search" 
+                        />
+
+                        <p className="font-bold">Meklēt</p>
+                    </div>
+                </Link>
+            </button>
+
             <button className="outline-green-600 p-1">
                 <Link href="/cart">
-                    <p className="font-bold">Grozs</p>
+                    <p className="font-bold hidden sm:block">Grozs</p>
+                </Link>
+
+                <Link href="/cart">
+                    <img 
+                        className="w-6 block sm:hidden"
+                        src="/svg/cart.svg" 
+                        alt="shopping cart" 
+                    />
                 </Link>
             </button>
         </div>
