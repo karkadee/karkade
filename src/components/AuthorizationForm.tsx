bf834799dbcaf0373ae3f551d002011ae50d7ee6
Rob <robzlegz@gmail.com>
2022-02-06 20:29:49 +0200

    name

--- src/components/AuthorizationForm.tsx
+++ src/components/AuthorizationForm.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 
@@ -7,9 +8,25 @@ const AuthorizationForm: React.FC = () => {
     const [type] = useState(router.pathname);
 
     return (
-        <div className="h-full flex items-center justify-center w-full">
-            <form className="w-80 bg-white flex flex-col items-center rounded-md py-4 px-4">
+        <div className="flex items-center justify-center w-full p-0 h-full">
+            <form className="w-11/12 max-w-[320px] bg-white flex flex-col items-center rounded-lg py-4 px-4">
                 <h2 className="mb-4">{type === "/auth/login" ? "Ieiet" : "Reģistrēties"}</h2>
+
+                {
+                    type === "/auth/register" && (
+                        <div className="flex flex-col w-full mb-4">
+                            <label className="mb-1">Vārds</label>
+
+                            <input 
+                                className="h-8"                        
+                                type="text" 
+                                placeholder="vārds"
+                                name="name" 
+                                id="name" 
+                            />
+                        </div>
+                    )
+                }
 
                 <div className="flex flex-col w-full mb-4">
                     <label className="mb-1">Epasts</label>
@@ -35,9 +52,22 @@ const AuthorizationForm: React.FC = () => {
                     />
                 </div>
 
-                <button 
-                    type="submit"
-                >{type === "/auth/login" ? "Ieiet" : "Reģistrēties"}</button>
+                <div className="flex flex-row w-full justify-between">
+                    <button 
+                        className="outline-green-600 py-1 px-6 rounded-md bg-green-600 text-white hover:bg-green-700"
+                        type="submit"
+                    >{type === "/auth/login" ? "Ieiet" : "Reģistrēties"}</button>
+
+                    <div className="flex items-center">
+                        <p>{type === "/auth/login" ? "Nav konta?" : "Jau ir konts?"}</p>
+
+                        <button>
+                            <Link href={type === "/auth/login" ? "/auth/register" : "/auth/login"}>
+                                <p>{type === "/auth/login" ? "Reģistrējies" : "Ielogojies"}</p>
+                            </Link>
+                        </button>
+                    </div>
+                </div>
             </form>
         </div>
     );
