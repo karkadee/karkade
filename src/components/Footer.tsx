36c057aa024d986fd120d542331a1ce7fd73d367
Rob <robzlegz@gmail.com>
2022-02-06 12:27:55 +0200

    accessibility

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,104 +4,104 @@ import React from "react";
 function Footer() {
     return (
         <footer className="w-full bg-white mt-20 flex flex-col md:flex-row items-center md:items-start justify-center py-4 px-0 md:px-10 lg:px-20">
-            <div className="md:mr-10 mb-4 pl-4 sm:pl-0 w-full sm:w-80 md:w-auto">
+            <div className="md:mr-10 mb-4 pl-4 sm:pl-0 w-full sm:w-80 md:w-auto flex flex-col items-start">
                 <strong className="">MŪSU KOMPĀNIJA</strong>
 
                 <button>
                     <Link href="/">
-                        <p className="">Par mums</p>
+                        <p className="text-left">Par mums</p>
                     </Link>
                 </button>
 
                 <button>
                     <Link href="/">
-                        <p className="">Mūsu veikali</p>
+                        <p className="text-left">Mūsu veikali</p>
                     </Link>
                 </button>
 
                 <button>
                     <Link href="/">
-                        <p className="">Privātuma politika</p>
+                        <p className="text-left">Privātuma politika</p>
                     </Link>
                 </button>
 
                 <button>
                     <Link href="/">
-                        <p className="">Lietošanas noteikumi un nosacījumi</p>
+                        <p className="text-left">Lietošanas noteikumi un nosacījumi</p>
                     </Link>
                 </button>
 
                 <button>
                     <Link href="/">
-                        <p className="">Juridisks paziņojums</p>
+                        <p className="text-left">Juridisks paziņojums</p>
                     </Link>
                 </button>
 
                 <button>
                     <Link href="/">
-                        <p className="">Kontakti</p>
+                        <p className="text-left">Kontakti</p>
                     </Link>
                 </button>
             </div>
 
-            <div className="md:mx-10 mb-4 pl-4 sm:pl-0 w-full sm:w-80 md:w-auto">
+            <div className="md:mx-10 mb-4 pl-4 sm:pl-0 w-full sm:w-80 md:w-auto flex flex-col items-start">
                 <strong className="">JŪSU PROFILS</strong>
 
                 <button>
                     <Link href="/">
-                        <p className="">Personīgā informācija</p>
+                        <p className="text-left">Personīgā informācija</p>
                     </Link>
                 </button>
 
                 <button>
                     <Link href="/">
-                        <p className="">Pasūtījumi</p>
+                        <p className="text-left">Pasūtījumi</p>
                     </Link>
                 </button>
 
                 <button>
                     <Link href="/">
-                        <p className="">Kredīta izraksti</p>
+                        <p className="text-left">Kredīta izraksti</p>
                     </Link>
                 </button>
             </div>
 
-            <div className="md:ml-10 mb-4 pl-4 sm:pl-0 w-full sm:w-80 md:w-auto">
+            <div className="md:ml-10 mb-4 pl-4 sm:pl-0 w-full sm:w-80 md:w-auto flex flex-col items-start">
                 <strong className="">VEIKALA INFORMĀCIJA</strong>
 
                 <button>
                     <Link href="/">
-                        <p className="">Karkadē</p>
+                        <p className="text-left">Karkadē</p>
                     </Link>
                 </button>
 
                 <button>
                     <Link href="/">
-                        <p className="">SIA "Karkadē"</p>
+                        <p className="text-left">SIA "Karkadē"</p>
                     </Link>
                 </button>
 
                 <button>
                     <Link href="/">
-                        <p className="">Tērbatas iela 19/21, Rīga, LV-1010</p>
+                        <p className="text-left">Tērbatas iela 19/21, Rīga, LV-1010</p>
                     </Link>
                 </button>
 
                 <button>
                     <Link href="/">
-                        <p className="">Reg.nr.50103327811</p>
+                        <p className="text-left">Reg.nr.50103327811</p>
                     </Link>
                 </button>
 
                 <button>
                     <Link href="/">
-                        <p className="">Telefona nr.: +37126621676</p>
+                        <p className="text-left">Telefona nr.: +37126621676</p>
                     </Link>
                 </button>
 
                 <button>
                     <Link href="/">
-                        <p className="">E-pasts: <a href="mailto: karkade@karkade.lv">karkade@karkade.lv</a></p>
+                        <p className="text-left">E-pasts: <a href="mailto: karkade@karkade.lv">karkade@karkade.lv</a></p>
                     </Link>
                 </button>
             </div>
