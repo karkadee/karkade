1a82af53a610333b1355e6e889b4cc34d27b3505
RobzLegz <robzlegz@gmail.com>
2022-02-11 21:47:41 +0200

    clogs

--- src/requests/authRequests.ts
+++ src/requests/authRequests.ts
@@ -29,11 +29,10 @@ const register = async (name: string, email: string, password: string, phoneNumb
     await axios.post(REGISTER_ROUTE, data)
         .then(async (res) => {
             const rf_token = res.data.refresh_token;
-            await localStorage.saveItem("rf_token", rf_token);
-
-            console.log(rf_token)
+            await window.localStorage.saveItem("rf_token", rf_token);
 
             await getAccessToken(dispatch);
+            setLoading(false);
         }).catch(async (err) => {
             setLoading(false);
             const error: string = err?.response?.data?.err;
@@ -64,9 +63,12 @@ const loginUser = async (email: string, password: string, dispatch: any, loading
     axios.post(LOGIN_ROUTE, data)
         .then(async (res) => {
             const rf_token = res.data.refresh_token;
-            await localStorage.saveItem("rf_token", rf_token);
+            console.log(rf_token)
+
+            await window.localStorage.saveItem("rf_token", rf_token);
 
             await getAccessToken(dispatch);
+            setLoading(false);
         }).catch(async (err) => {
             setLoading(false);
             const error: string = err?.response?.data?.err;
@@ -83,7 +85,7 @@ const loginUser = async (email: string, password: string, dispatch: any, loading
 }
 
 const getAccessToken = async (dispatch: Dispatch) => {
-    const rf_token = localStorage.getItem("rf_token");
+    const rf_token = window.localStorage.getItem("rf_token");
     console.log(rf_token)
 
     if(!rf_token){
